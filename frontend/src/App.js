ab4a144a885e1c6bfe19df795e4e4cedaaaadd0f
SPLEENINJA <capeljules@gmail.com>
2025-10-29 16:23:49 +0100

    Frontend ready

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App" style={{minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+      <main style={{textAlign: 'center'}}>
+        <h1 style={{marginBottom: '0.5rem'}}>Bienvenue sur GameForge</h1>
+        <p style={{marginTop: 0}}>Page d'accueil</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
